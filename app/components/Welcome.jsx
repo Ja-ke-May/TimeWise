20b15d05b836b5dc55c9686eaec23bf5859dac4c
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-03-07 21:25:11 +0000

    added loading image and 2nd video type iption

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -36,7 +36,7 @@ const Welcome = ({ onWelcomeFinish, quizType, containerBorder }) => {
   return (
     <div className={`relative mt-20 mb-20 ml-5 mr-5 border-2 ${containerBorder}`}>
       <div id="welcome-container" className="p-6 bg-black bg-opacity-60 rounded mx-auto text-center text-white md:max-w-2xl">
-        <div className="text-2xl flex items-center justify-center inset-x-0">
+        <div className="text-3xl flex items-center justify-center inset-x-0">
           Welcome to TimeWise
         </div>
         <hr className={`m-3 ${hrColor}`} />
