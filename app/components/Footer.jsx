774860175935d4bee0b4140aa4e78836c106ead8
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-05-24 18:30:50 +0100

    insta removed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -13,7 +13,6 @@ const Footer = () => {
   return (
     <div id="footer" className="relative flex flex-col w-full bg-black/80 text-white text-center text-opacity-60 justify-center items-center mt-40 pt-5 pb-5 pr-1 pl-1">
       <img className='h-20 w-20 motion-safe:animate-pulse' src="images/TimeWiseLogo.png" alt="TimeWise Logo" />
-      <a href="https://www.instagram.com/timewisequiz/" title="Instagram"><img className='h-12 w-12 m-4 mt-5 opacity-60 hover:border-4 hover:rounded-full hover:border-pink-500 hover:bg-pink-500/50' src="images/instagram-icon.png" alt="Instagram" /></a>
       <a href="mailto:info@timewisequiz.com" className="hover:text-pink-500 text-2xl block mt-2" title='email'>info@timewisequiz.com</a>
       <hr className="border-pink-500 w-1/2 m-4" />
 
